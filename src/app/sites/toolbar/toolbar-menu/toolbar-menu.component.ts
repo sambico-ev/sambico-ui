f487b72eadc79fb3f48a6cbd91f9f96684de96d8
dellMarco <marco.delloso@gmail.com>
2023-12-13 19:23:39 +0100

    mobile styling fixes

--- src/app/sites/toolbar/toolbar-menu/toolbar-menu.component.ts
+++ src/app/sites/toolbar/toolbar-menu/toolbar-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouteNames } from 'src/app/app-routing.module';
+import { BreakpointService } from 'src/app/shared/services/breakpoint.service';
 
 @Component({
   selector: 'app-toolbar-menu',
@@ -8,6 +9,9 @@ import { RouteNames } from 'src/app/app-routing.module';
 })
 export class ToolbarMenuComponent {
   routes = RouteNames;
+  isMobile = this.breakpointService.isMobile();
+
+  constructor(private readonly breakpointService: BreakpointService) {}
 
   downloadMembershipApplication(): void {
     const link = document.createElement('a');
