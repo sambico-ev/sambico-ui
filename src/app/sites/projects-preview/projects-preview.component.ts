e91637c07e8f5c036b1894566aa2cb522b4e1339
dellMarco <marco.delloso@gmail.com>
2024-01-18 10:38:40 +0100

    fix procet preview title

--- src/app/sites/projects-preview/projects-preview.component.ts
+++ src/app/sites/projects-preview/projects-preview.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouteNames } from 'src/app/app-routing.module';
 import { Project } from 'src/app/shared/models/strapi.models';
 import { StrapiService } from 'src/app/shared/services/strapi.service';
+import { UtilsService } from 'src/app/shared/services/utils.service';
 
 @Component({
   selector: 'app-projects-preview',
@@ -12,11 +13,14 @@ export class ProjectsPreviewComponent implements OnInit {
   projects: Project[] = [];
   routes = RouteNames;
 
-  constructor(public strapiService: StrapiService) {}
+  constructor(
+    public strapiService: StrapiService,
+    private readonly utilService: UtilsService
+  ) {}
 
   ngOnInit(): void {
-    this.strapiService.getProjects(2).subscribe((res) => {
-      this.projects = res;
+    this.strapiService.getProjects().subscribe((res) => {
+      this.projects = this.utilService.shuffleArray<Project>(res).splice(0, 1);
     });
   }
 }
