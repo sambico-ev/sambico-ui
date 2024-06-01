0a36407394393eaf4d778535f65cdb87c4f4b8e5
Marco Dell'Oso <marco.delloso@gmail.com>
2024-06-01 16:04:14 +0200

    add more social links

--- src/app/sites/footer/footer.module.ts
+++ src/app/sites/footer/footer.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from './footer.component';
-import { TranslateModule } from '@ngx-translate/core';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { SharedModule } from '../../shared/shared.module';
+import { FooterComponent } from './footer.component';
 
 @NgModule({
   declarations: [FooterComponent],
-  imports: [CommonModule, TranslateModule, RouterModule],
+  imports: [CommonModule, TranslateModule, RouterModule, SharedModule],
   exports: [FooterComponent],
 })
 export class FooterModule {}
